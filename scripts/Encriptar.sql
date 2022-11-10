d64d6ab7283642712fa4e83506271a21e34602c5
AylanJ123 <aylanjosue@gmail.com>
2022-11-10 10:17:00 -0600

    Base de datos... de virus, ha sido actualizada :P

--- scripts/Encriptar.sql
+++ scripts/Encriptar.sql
@@ -1,49 +1,32 @@
-Go;
-create table usuario
+--insert into usuario values(3,'Admin',dbo.ENCRIPTA_PASS('PassAdmin'));
+--select dbo.desencriptar_pass('pG5P4zz') from usuario;
+
+use ProyectoG5;
+
+-- EJECUTAR 1 por 1
+create function ENCRIPTAR_PASS
 (
-id_usuario INT primary key,
-usuario varchar(20) not null,
-pass varbinary(500) not null
+	@clave varchar(50)
 )
+returns VarBinary(MAX)
+as
+	begin
+		declare @pass as VarBinary(MAX)
+		set @pass=ENCRYPTBYPASSPHRASE('pG5P4zz',@clave)
+		return @pass
+	end
+-- EJECUTAR HASTA AQUÍ
 
-go;
-alter function ENCRIPTA_PASS
- (
- @clave varchar(50)
- )
- returns VarBinary(500)
- as
- begin
- declare @pass as VarBinary(500)
- set @pass=ENCRYPTBYPASSPHRASE('MiclaveSecreta',@clave)
- return @pass
- end
-
-
- go;
-alter function desencriptar_pass
-  (
-  @clave varbinary(500)
-  )
-  returns varchar(50)
-  as
-  begin
-  declare @pass as varchar(50)
-  set @pass=DECRYPTBYPASSPHRASE('MiclaveSecreta',@clave)
-  return @pass
-  end
-
-delete from usuario
-
-insert into usuario values(1,'Geovanny Chacon',dbo.ENCRIPTA_PASS('PassSistema'))
-insert into usuario values(2,'Pedro Castro',dbo.ENCRIPTA_PASS('123456'))
-insert into usuario values(3,'Admin',dbo.ENCRIPTA_PASS('PassAdmin'))
-
-
-Select * from usuario
-select id_usuario,usuario,dbo.desencriptar_pass(pass) from usuario
-
-Select * from usuario
-
-
-SELECT c.Cod_Provincia, c.Cod_Canton, c.DSC_Canton, c.LOG_Activo from Canton c
+-- EJECUTAR 1 por 1
+create function DESENCRIPTAR_PASS
+(
+	@clave varbinary(MAX)
+)
+returns varchar(50)
+as
+	begin
+		declare @pass as varchar(50)
+		set @pass=DECRYPTBYPASSPHRASE('pG5P4zz',@clave)
+		return @pass
+	end
+-- EJECUTAR HASTA AQUÍ
